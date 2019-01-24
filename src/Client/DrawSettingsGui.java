d39f8f7cf91d3c0ef8c19c046a1576a74f9187b2
Evan Jesty <evanjesty@gmail.com>
2019-01-23 22:17:25 -0500

    Use maven for easier install

--- src/Client/DrawSettingsGui.java
+++
@@ -1,7 +0,0 @@
-package Client;
-
-/**
- * Created by Evan on 7/3/2017.
- */
-public class DrawSettingsGui {
-}
--- src/Jesty/test/testTcpbridge.java
+++
@@ -1,7 +0,0 @@
-package Jesty.test;
-
-/**
- * Created by Evan on 12/13/2016.
- */
-public class testTcpbridge {
-}
